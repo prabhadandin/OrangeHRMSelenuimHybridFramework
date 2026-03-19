2e1c698199fdf5f6c7d6473f5721170446cf4af6
Prabha <Prabha@HP>
2026-03-19 13:19:09 +1300

    Enhance CI workflow for Node.js 24 and parallel Selenium tests

--- obj/Debug/net8.0/OrangeHRMHybridAutomationFramework.AssemblyInfo.cs
+++ obj/Debug/net8.0/OrangeHRMHybridAutomationFramework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrangeHRMHybridAutomationFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed799050bb978c853203a48782f2e4111255c4d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db868d1fb59732bc5013f3d5b49bc72a466b51e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrangeHRMHybridAutomationFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrangeHRMHybridAutomationFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
